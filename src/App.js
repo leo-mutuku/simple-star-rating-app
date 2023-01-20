c2c2ad4c179625fc7376a2389a8404e5797f0726
leo-mutuku <le.mutuku@gmail.com>
2023-01-20 20:29:24 +0300

    simple star rating app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import StarRating from "./StarRatig"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <h1>Simple Start Rating</h1>
+      <p>React props, state and callback
+        <a href="#" target="blank">&nbsp;learn more.
         </a>
-      </header>
-    </div>
+      </p>
+    <br/>
+</div >
   );
 }
 
---
+++ src/StarRatig.js
@@ -0,0 +1,46 @@
+import * as React from 'react';
+
+// functional component star rating
+function StarRating() {
+
+const [stars, setStars] = React.useState([]);
+const [rating, setRating] = React.useState(0);
+const [hovered, setHovered] = React.useState(0);
+const [selectedIcon, setSelectedIcon] = React.useState("★")
+const [deselectedIcon, setDeselectedIcon] = React.useState("☆")
+
+let outOf = 5;
+
+for(let i = 0 ; i < outOf; i++){
+    setStars(i + 1)
+}
+
+
+return (
+    <div>
+                <div className="rating" style={{ fontSize: '5em', color: "#38d39f" }}>
+
+                    {stars.map(star => {
+                        return (
+                            <span
+                                style={{ cursor: 'pointer' }}
+                                onClick={() => { this.changeRating(star); }}
+                                onMouseEnter={() => { this.hoverRating(star); }}
+                                onMouseLeave={() => { this.hoverRating(0); }}
+                            >
+                                {rating < star ?
+                                    hovered < star ? deselectedIcon : selectedIcon
+                                    :
+                                    selectedIcon
+                                }
+                            </span>
+                        );
+                    })}
+
+                </div>
+            </div>
+)
+
+}
+
+export default StarRating
